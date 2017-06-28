395d0c0feddd4f4133c255f6a3314831826824ed
Maxim Rubis <siburny@gmail.com>
2017-06-28 19:46:29 -0400

    Expose 8887 and 8888 ports

--- restart.ps1
+++ restart.ps1
@@ -1,3 +1,2 @@
-docker stop steward-dev
-docker rm steward-dev
-docker run -d -v c:\Projects\nodejs\steward-docker\db:/usr/src/app/db --net host --name steward-dev siburny/steward-docker
+.\stop.ps1
+.\start.ps1
