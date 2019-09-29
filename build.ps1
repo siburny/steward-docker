548cbead5ef751f882cacc572e687c89a2f3b938
Maxim Rubis <siburny@gmail.com>
2019-09-29 10:28:32 -0400

    use docker subfolder; add new public keys

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 # Clear stale artifacts
-try { rm -force -r steward-master -erroraction stop } Catch { }
+try { rm -force -r docker/steward-master -erroraction stop } Catch { }
 try { rm master.zip -erroraction stop } Catch { }
 
 # Download archive
@@ -11,11 +11,11 @@ $WebClient.DownloadFile("https://github.com/siburny/steward/archive/master.zip",
 
 # Extract archive
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory("master.zip", ".\")
+[System.IO.Compression.ZipFile]::ExtractToDirectory("master.zip", "./docker/")
 
 # Build image
-docker build -t siburny/steward .
+docker build -t siburny/steward docker/.
 
 # Clear stale artifacts
-try { rm -force -r steward-master -erroraction stop } Catch { }
+try { rm -force -r docker/steward-master -erroraction stop } Catch { }
 try { rm master.zip -erroraction stop } Catch { }
